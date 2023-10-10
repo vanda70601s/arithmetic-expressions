1b9e743b622f262f95eabb18c898aafb26bd07aa
Work <vanda70601s@mail.ru>
2023-10-10 23:21:01 +0300

    Commit

--- src/main/java/com/epam/rd/autotasks/arithmeticexpressions/Expressions.java
+++ src/main/java/com/epam/rd/autotasks/arithmeticexpressions/Expressions.java
@@ -1,37 +1,109 @@
 package com.epam.rd.autotasks.arithmeticexpressions;
 
-import java.util.StringJoiner;
 
 public class Expressions {
 
     public static Variable var(String name, int value) {
         //implement this method
-        return null;
+        return new Variable(name, value);
     }
 
     public static Expression val(int value) {
-        //implement this method
-        return null;
+        return new Expression() {
+            @Override
+            public int evaluate() {
+                return value;
+            }
+
+            @Override
+            public String toExpressionString() {
+                if (value< 0) return "(" + value + ")";
+                else return String.valueOf(value);
+            }
+        };
     }
 
-    public static Expression sum(Expression... members){
+    public static Expression sum(Expression... members) {
         //implement this method
-        return null;
+        if (members.length < 2) return null;
+
+        return new Expression() {
+            @Override
+            public int evaluate() {
+               int result = 0;
+                for (Expression mem : members) {
+                    result += mem.evaluate();
+                }
+                return result;
+            }
+
+            @Override
+            public String toExpressionString() {
+                String s = "(";
+                for (int i = 0; i < members.length - 1; i++) {
+                    s = s + members[i].toExpressionString() + " + ";
+                }
+                s = s + members[members.length - 1].toExpressionString() + ")";
+                return s;
+            }
+        };
     }
 
-    public static Expression product(Expression... members){
-        //implement this method
-        return null;
+
+    public static Expression product(Expression... members) {
+        if (members.length < 2) return null;
+
+        return new Expression() {
+            @Override
+            public int evaluate() {
+                int result = 1;
+                for (Expression mem : members) {
+                    if (mem.evaluate() != 0)
+                        result *= mem.evaluate();
+                }
+                return result;
+            }
+
+            @Override
+            public String toExpressionString() {
+                String s = "(";
+                for (int i = 0; i < members.length - 1; i++) {
+                    s = s + members[i].toExpressionString() + " * ";
+                }
+                s = s + members[members.length - 1].toExpressionString() + ")";
+                return s;
+            }
+        };
     }
 
-    public static Expression difference(Expression minuend, Expression subtrahend){
-        //implement this method
-        return null;
+    public static Expression difference(Expression minuend, Expression subtrahend) {
+
+        return new Expression() {
+            @Override
+            public int evaluate() {
+                return minuend.evaluate() - subtrahend.evaluate();
+            }
+
+            @Override
+            public String toExpressionString() {
+                return "(" + minuend.toExpressionString() + " - " + subtrahend.toExpressionString() + ")";
+            }
+        };
     }
 
-    public static Expression fraction(Expression dividend, Expression divisor){
+    public static Expression fraction(Expression dividend, Expression divisor) {
         //implement this method
-        return null;
+        return new Expression() {
+            @Override
+            public int evaluate() {
+                return dividend.evaluate() / divisor.evaluate();
+            }
+
+            @Override
+            public String toExpressionString() {
+                return "(" + dividend.toExpressionString() + " / " + divisor.toExpressionString() + ")";
+            }
+        };
     }
 
 }
--- src/main/java/com/epam/rd/autotasks/arithmeticexpressions/Variable.java
+++ src/main/java/com/epam/rd/autotasks/arithmeticexpressions/Variable.java
@@ -2,23 +2,28 @@ package com.epam.rd.autotasks.arithmeticexpressions;
 
 public class Variable implements Expression {
 
+    private final String name;
+    private int value;
+
     public Variable(String name, int value) {
-        //implement this method
+       this.name = name;
+       this.value = value;
+       //implement this method
     }
 
     public void setValue(int value) {
-        //implement this method
+        this.value = value;
     }
 
     @Override
     public int evaluate() {
         //implement this method
-        return 0;
+        return value;
     }
 
     @Override
     public String toExpressionString() {
         //implement this method
-        return null;
+        return name;
     }
 }
